8105d8bcd25dc5a01bb7a97146b918276258f023
Máté Molnár <doglotteger@gmail.com>
2018-03-04 17:28:01 +0100

    add some demo to unit testing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,85 +2,7 @@
   <div class="hello">
     <router-link to="/game">Go to game</router-link>
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h2 class="counter">Count: {{ count }}</h2><button @click="increment">Increment</button>
   </div>
 </template>
 
@@ -90,8 +12,14 @@ export default {
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
+      count: 0
     };
   },
+  methods: {
+    increment() {
+      this.count += 1;
+    }
+  }
 };
 </script>
 
--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,34 @@
-import Vue from 'vue';
+import { shallow } from 'vue-test-utils';
 import HelloWorld from '@/components/HelloWorld';
 
 describe('HelloWorld.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(HelloWorld, {
+      stubs: ['router-link']
+    });
+  });
+
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    expect(wrapper.find('.hello h1').text())
       .to.equal('Welcome to Your Vue.js App');
+  });
+
+  it('should have initial count of 0', () => {
+    expect(wrapper.vm.count).to.equal(0);
+  });
+
+  describe('increment', () => {
+    it('should increment the count', () => {
+      wrapper.vm.increment();
+      expect(wrapper.vm.count).to.equal(1);
+    });
+
+    it('should show the incremented count after clicking the button', () => {
+      const button = wrapper.find('button');
+      button.trigger('click');
+      expect(wrapper.find('.hello .counter').text()).to.contain('1');
+    });
   });
 });

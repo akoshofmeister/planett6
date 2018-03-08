a9eb6cdb677003ec345abbf4c483aff2fe0b11ca
akoshofmeister <h.akos@outlook.com>
2018-03-08 18:32:07 +0100

    Added game

---
+++ src/components/game/imageLoader.js
@@ -0,0 +1,170 @@
+function imageLoader() {
+    var imageLoader = {
+    };
+
+    var urls = [
+        "./images/background.png",
+        "./images/npc.png",
+        "./images/ground.png",
+        "./images/groundTp.png",
+        "./images/bullet.png",
+        "./images/player.png",
+        "./images/player2.png"
+    ];
+
+    var images = {};
+
+    var regexToCaptureName = /(^.*[\\\/])(.+)(.{4}$)/;
+    var loadImage = function(url) {
+        return new Promise(function(resolve, reject) {
+            try {
+                var tmp = new Image();
+                tmp.src = url;
+
+                tmp.onload = function() {
+                    images[url.replace(regexToCaptureName, "$2")] = tmp;
+                    resolve();
+                }
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    imageLoader.loadAll = function() {
+        return new Promise(function(resolve, reject) {
+            Promise.all( 
+                urls.reduce(
+                    (res, url) => { res.push(loadImage(url)); return res; }
+                    , []) 
+            )
+            .then(function() {
+                resolve();
+            })  
+            .catch(function(err) {
+                console.log(err);
+                reject();
+            })
+        })
+    }
+
+    imageLoader.get = function(what, details) {
+        details = details instanceof Array ? details : [details];
+        switch(what) {
+            case "ground":
+                return groundLoader(details);
+            case "player":
+                return playerLoader(details);
+            case "player2":
+                return playerLoader(details, true)
+            case "npc":
+                return npcLoader(details);
+            case "bullet":
+                return bulletLoader(details);
+            case "background":
+                return backgroundLoader(details);
+            case "navigation":
+                return navigationLoader(details);
+        }
+
+        return {x:0, y:0, image: null};
+    }
+
+    let bulletLoader = function(details) {
+        let rO = {};
+        rO.x = (details[1] || 0) * GAME.sizes.blockWidth;
+        rO.y = (details[0] == "forward" ? 0 : 1) * GAME.sizes.blockHeight;
+        rO.image = images.bullet;
+
+        return rO;
+    }
+
+    let npcLoader = function(details) {
+        let rO = {};
+        rO.x = 0;
+        rO.y = 0;
+        rO.image = images.npc;
+        
+        switch(details[1]) {
+            case "walk":
+                rO.x = (details[2] || 0) * GAME.sizes.blockWidth;
+                rO.y = (details[0] == "forward" ? 1 : 2) * GAME.sizes.blockHeight;
+                break;
+            case "climbUp":
+                rO.x = (details[2] || 0) * GAME.sizes.blockWidth;
+                rO.x = rO.x == 0 ? 0 : rO.x + details[2] * GAME.sizes.blockWidth;
+                rO.y = (details[0] == "forward" ? 7 : 5) * GAME.sizes.blockHeight;
+                rO.height = 2 * GAME.sizes.blockHeight;
+                rO.width = 2 * GAME.sizes.blockWidth;
+                break;
+            break;
+            case "climbDown":
+                rO.x = (details[2] || 0) * GAME.sizes.blockWidth;
+                rO.x = rO.x == 0 ? 0 : rO.x + details[2] * GAME.sizes.blockWidth;
+                rO.y = (details[0] == "forward" ? 11 : 9) * GAME.sizes.blockHeight;
+                rO.height = 2 * GAME.sizes.blockHeight;
+                rO.width = 2 * GAME.sizes.blockWidth;
+                break;
+            break;
+            case "die":
+                rO.x = (details[2] || 0) * GAME.sizes.blockWidth;
+                rO.y = (details[0] == "forward" ? 4 : 3) * GAME.sizes.blockHeight;
+                break;
+            break;
+            default:
+                rO.x = (details[0] == "forward" ? 1 : 0) * GAME.sizes.blockWidth;
+            break;
+        }
+
+        return rO;
+    }
+
+    var groundLoader = function(details) {
+        var rO = {};
+        rO.x = 0;
+        rO.y = 0;
+        if(details[0] == "top") {
+            rO.image = images.groundTp;
+        } else {
+            rO.image = images.ground;
+        }
+
+        return rO;
+    }
+
+    var playerLoader = function(details, player2) {
+        var rO = {};
+        rO.image = player2 ? images.player2 : images.player;
+        
+        switch(details[1]) {
+            case "jump":
+                rO.y = 6 * GAME.sizes.blockHeight;
+                rO.x = (details[0] == "forward" ? 0 : 1) * GAME.sizes.blockWidth;
+                break;
+            case "die":
+                rO.y = 1 * GAME.sizes.blockHeight,
+                rO.x = (details[0] == "forward" ? 0 : 1) * GAME.sizes.blockWidth;
+                break;
+            case "stand":
+                rO.x = (details[2] || 0) * GAME.sizes.blockWidth;
+                rO.y = (details[0] == "forward" ? 2 : 3) * GAME.sizes.blockHeight;
+                break;
+            case "walk":
+                rO.x = (details[2] || 0) * GAME.sizes.blockWidth;
+                rO.y = (details[0] == "forward" ? 4 : 5) * GAME.sizes.blockHeight;
+                break;
+        }
+        
+        return rO;
+    }
+
+    var backgroundLoader = function(details) {
+        var rO = {};
+        rO.x = 0;
+        rO.y = 0;
+        rO.image = images.background;
+
+        return rO;
+    }
+    return imageLoader;
+}
---
+++ src/components/game/objects/Player.js
@@ -0,0 +1,155 @@
+function Player(player2) {
+    var imageType = player2 ? "player2" : "player";
+    var Player = {
+        type: "player",
+        currentImage: { index: { walk: 0, stand: 1 }, image: GAME.imageLoader.get(imageType, ["forward", "stand", "0"]) },
+        x: (player2 && 222) || 111,
+        y: 333,
+        direction: 1,
+        maxSpeed: { x: 500, y: 8 },
+        velocity: { x: 0, y: 0 },
+        acceleration: { x: 700, y: 0 },
+        moved: false,
+        player2: !!player2,
+        canShoot: true,
+        health: 3,
+        dead: false
+    };
+
+    var speedUp = function (forward, time) {
+        if (forward && this.velocity.x < this.maxSpeed.x) {
+            this.velocity.x = Math.min(this.velocity.x + this.acceleration.x * (this.velocity.x < 0 ? 2 : 1) * time, this.maxSpeed.x);
+        } else if (!forward && this.velocity.x > -this.maxSpeed.x) {
+            this.velocity.x = Math.max(this.velocity.x - this.acceleration.x * (this.velocity.x > 0 ? 2 : 1) * time, -this.maxSpeed.x);
+        }
+        this.moved = true;
+
+        if (!GAME.validPos(this.x + this.velocity.x * time, this.y, true) || !GAME.canGo(this.x + this.velocity.x * time, this.y, forward, true)) {
+            this.velocity.x = 0;
+        }
+    }
+
+    var slowDown = function (time) {
+        if (this.velocity.x > 0) {
+            this.velocity.x -= this.acceleration.x * 3 * time;
+
+            if (this.velocity.x < 0) {
+                this.velocity.x = 0;
+            }
+        } else if (this.velocity.x < 0) {
+            this.velocity.x += this.acceleration.x * 3 * time;
+
+            if (this.velocity.x > 0) {
+                this.velocity.x = 0;
+            }
+        }
+
+        if (!GAME.validPos(this.x + this.velocity.x * time, this.y, true) || !GAME.canGo(this.x + this.velocity.x * time, this.y, this.velocity.x > 0, true)) {
+            this.velocity.x = 0;
+        }
+    }
+
+    Player.move = function (time, keys) {
+        if(keys.shoot) {
+            if(this.canShoot) {
+                this.shoot();
+                this.canShoot = false;
+                
+                setTimeout(() => { this.canShoot = true; }, 500);
+            }
+        }
+
+        if (keys.forward || keys.backward) {
+            this.direction = keys.forward ? 1 : -1;
+        }
+
+        if (time) {
+
+            if (keys.up && this.velocity.y == 0) {
+                this.velocity.y = -20;
+                this.jumping = true;
+            }
+
+
+            if (keys.forward || keys.backward) {
+                speedUp.bind(this)(keys.forward, time);
+            } else if (this.velocity.x != 0) {
+                slowDown.bind(this)(time);
+            }
+
+            let canFall = GAME.canFall(this.x, this.y, true);
+            if (this.jumping || canFall) {
+                this.velocity.y += 1.7;
+            }
+
+
+            this.y += this.velocity.y;
+
+            if (this.velocity.y > 0 && !canFall) {
+                this.y = GAME.getGround(this.x, true).y;
+                this.velocity.y = 0;
+                this.jumping = false;
+            }
+
+            this.x += this.velocity.x * time;
+
+            let wrongPosition = GAME.isWrongPosition(this.x, this.y, true);
+            if ((canFall || this.velocity.x == 0) && wrongPosition) {
+                let goodPos = GAME.getGoodPosition(this.x, this.y);
+                let xToBe = this.x + wrongPosition * GAME.sizes.blockWidth * 0.1;
+
+                if (
+                    (wrongPosition == 1 && xToBe > goodPos) ||
+                    (wrongPosition == -1 && xToBe < goodPos)
+                ) {
+                    this.x = goodPos;
+                } else {
+                    this.x = xToBe;
+                }
+            }
+        }
+    }
+
+    Player.die = function() {
+
+    }
+
+    Player.shoot = function() {
+        GAME.bullets.push( new Bullet(this.x, this.y, this.direction, this.player2) );
+    }
+
+    Player.getMove = function (time) {
+        if (GAME.canFall(this.x, this.y, true)) {
+            this.currentImage.index.walk = -1;
+            this.currentImage.index.stand = -1;
+            this.currentImage.image = GAME.imageLoader.get(imageType, [(this.direction == 1 ? "forward" : "backward"), "jump"]);
+        } else {
+            if (this.moved) {
+                this.moved = false;
+                this.currentImage.index.stand = -1;
+
+                if (++this.currentImage.index.walk > 3) {
+                    this.currentImage.index.walk = 0;
+                }
+
+                this.currentImage.image = GAME.imageLoader.get(imageType, [(this.direction == 1 ? "forward" : "backward"), "walk", this.currentImage.index.walk]);
+            } else {
+                this.currentImage.index.walk = -1;
+
+                if (++this.currentImage.index.stand > 3) {
+                    this.currentImage.index.stand = 0;
+                }
+
+                this.currentImage.image = GAME.imageLoader.get(imageType, [(this.direction == 1 ? "forward" : "backward"), "stand", this.currentImage.index.stand]);
+            }
+        }
+
+        return {
+            x: this.x,
+            y: this.y,
+            image: this.currentImage.image
+        }
+    }
+
+    return Player;
+}

0f49b20a305a0f1c8d37777a988749e8e30f97f5
mmolnar <mmolnar@3amlabs.net>
2018-04-19 12:44:35 +0200

    fix service registration order in container

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,9 +5,10 @@ import { InversifyExpressServer } from 'inversify-express-utils';
 import { connect as mongooseConnect, Model } from 'mongoose';
 
 import { serverContainer } from './container/container';
+import './service/services.module';
+
 import './controller/health.controller';
 import './controller/user.controller';
-import './service/services.module';
 
 export const server = new InversifyExpressServer(serverContainer);
 
